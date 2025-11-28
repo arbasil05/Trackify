1b9aeda3f3db07171cc52e6de9902c74235bbb50
Visalan H <visalanprivate@gmail.com>
2025-11-28 10:41:29 +0530

    loading spinner added

--- Frontend/src/pages/User.jsx
+++ Frontend/src/pages/User.jsx
@@ -5,36 +5,40 @@ import axios from "axios";
 import UserHeader from "../components/userheader/UserHeader";
 import UserDetails from "../components/userdetails/UserDetails";
 import SemDetails from "../components/semDetails/SemDetails";
+import Spinner from "../components/spinner/Spinner";
 
 const User = ({ isDark, setIsDark, onDataRefresh }) => {
 
     const [userDetails, setUserDetails] = useState({});
-    const [userSem,setUserSem] = useState({});
-    const [loading,setLoading] = useState(false);
+    const [userSem, setUserSem] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setLoading(true);
         axios.get(`${import.meta.env.VITE_BACKEND_API}/api/user/courseByUser`, { withCredentials: true })
             .then((res) => {
-                const { user,user_sem_credits } = res.data;
+                const { user, user_sem_credits } = res.data;
                 setUserDetails(user);
                 setUserSem(user_sem_credits);
             })
-            .catch((error) => {
-                // console.log(error);
-            })
-            .finally(()=>{
+            .catch(() => { })
+            .finally(() => {
                 setLoading(false)
             })
 
     }, [])
+
+    if (loading) {
+        return <Spinner isDark={isDark} message="Loading profile..." />;
+    }
+
     return (
         <div>
             <Sidebar dark={isDark} />
             <Navbar dark={isDark} setIsDark={setIsDark} name={userDetails.name} />
-            <UserHeader isDark={isDark}/>
-            <UserDetails isDark={isDark} userDetails={userDetails} loading={loading} onDataRefresh={onDataRefresh} />
-            <SemDetails isDark={isDark} userSem={userSem} loading={loading} onDataRefresh={onDataRefresh} />
+            <UserHeader isDark={isDark} />
+            <UserDetails isDark={isDark} userDetails={userDetails} onDataRefresh={onDataRefresh} />
+            <SemDetails isDark={isDark} userSem={userSem} onDataRefresh={onDataRefresh} />
         </div>
     )
 }
