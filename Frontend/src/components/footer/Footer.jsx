1695ab2a295120dc484381b256a3d082cad38fe2
Visalan H <visalanprivate@gmail.com>
2026-02-02 00:03:29 +0530

    feat - modify seo in html and add gitrid in footer

--- Frontend/src/components/footer/Footer.jsx
+++ Frontend/src/components/footer/Footer.jsx
@@ -1,20 +1,35 @@
 import { Link } from 'react-router-dom'
 import './Footer.css'
 import Lottie from 'lottie-react';
-import bee from '../../assets/lottie/Bee2.json';
 import bee2 from '../../assets/lottie/Bee.json';
+import gitrid from '../../assets/lottie/gitrid.json';
+import { useTheme } from '../../context/ThemeContext';
 
 const Footer = () => {
+  const { isDark } = useTheme();
   return (
     <div className='footer-container'>
       <p>Have an issue ? <Link to="/feedback"><span className='contact-us'>Contact Us</span></Link></p>
       <div className='footer-bee'>
-        <div className="bee-tooltip">
-          Also try <a href="https://quick-qb.vercel.app/" target="_blank" rel="noopener noreferrer">QuickQB</a>
+        
+        <div className="icon-wrapper">
+          <div className="bee-tooltip">
+            Also try <a href="https://quick-qb.vercel.app/" target="_blank" rel="noopener noreferrer">QuickQB</a>
+          </div>
+          <a href="https://quick-qb.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <Lottie animationData={bee2} loop autoplay />
+          </a>
         </div>
-        <a href="https://quick-qb.vercel.app/" target="_blank" rel="noopener noreferrer">
-          <Lottie animationData={bee2} loop autoplay />
-        </a>
+
+        <div className="icon-wrapper">
+          <div className="bee-tooltip">
+            Also try <a href="https://gitrid.vercel.app/" target="_blank" rel="noopener noreferrer">GitRid</a> 
+          </div>
+          <a href="https://gitrid.vercel.app/" target="_blank" rel="noopener noreferrer" className={isDark ? 'gitrid-icon' : ''}>
+            <Lottie animationData={gitrid} loop autoplay />
+          </a>
+        </div>
+
       </div>
     </div>
   )
